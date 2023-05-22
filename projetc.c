d723042e1e6c478deb2d8c370ed62e22430f43cb
Gabriel LAURENT <glaurent@cesium.vav.sebetln.net>
2023-05-22 18:35:05 +0000

    Version 2

--- projetc.c
+++ projetc.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
-
+#include <string.h>
 
 #define base 10
 #define tailleMax 1000
@@ -15,14 +15,46 @@ typedef struct {
 
 //PROTOTYPES
 
+GrandEntier soustraction(GrandEntier a, GrandEntier b);
+GrandEntier negative(GrandEntier a);
+GrandEntier soustraction_sans_signe(GrandEntier a, GrandEntier b);
+int maximum(int a, int b);
 int egal_a(GrandEntier a, GrandEntier b);
 int different_de(GrandEntier a, GrandEntier b);
 int inferieur_a(GrandEntier a, GrandEntier b);
 int inferieur_ou_egal_a(GrandEntier a, GrandEntier b);
 void ordre(GrandEntier a, GrandEntier b, GrandEntier c);
-void additionner(GrandEntier a, GrandEntier b, GrandEntier c);
-void soustraire(GrandEntier a, GrandEntier b, GrandEntier c);
 
+GrandEntier lecture_grand_entier(const char* str){
+        GrandEntier r = {0};
+
+        const char *p = str;
+
+        r.positif = true;
+
+        if (*p == '+' || *p == '-') {
+                if (*p == '-') {
+                        r.positif = false;
+                }
+                p++;
+        }
+        while (*p != 0 && *p == '0') {
+                p++;
+        }
+
+        if (*p != 0) {
+                r.taille = strlen(p);
+
+                for (int i = 0 ; i < r.taille ; i++) {
+                        r.digit[r.taille-1-i] = p[i] - '0';
+                }
+        } else {
+                r.taille=1;
+                r.positif=true;
+        }
+
+        return r;
+}
 
 
 void afficher_grand_entier(GrandEntier entier) {                // Afficher le signe du grand entier
@@ -43,315 +75,201 @@ void afficher_grand_entier(GrandEntier entier) {                // Afficher le s
     printf("\n");
 }
 
-void addition(GrandEntier a, GrandEntier b){
+GrandEntier addition(GrandEntier a, GrandEntier b){
 
     GrandEntier c = {0};
-    c.positif = true;
 
-    if(a.positif == 1 && b.positif == 1){
-        additionner(a, b, c);
-
-    }
-
-    else if (a.positif == 0 && b.positif == 0){
-        c.positif = false;
-        additionner(a, b, c);
-    }
-
-    else if (a.positif == 1 && b.positif == 0){
-        ordre(a, b, c);
-    }
-
-    else if (a.positif == 0 && b.positif == 1){
-        ordre(b, a, c);
-    }
-
-}
-
-void additionner(GrandEntier a, GrandEntier b, GrandEntier c){
-
-
+    int n = maximum(a.taille, b.taille);
     int retenue = 0;
 
-    if (a.taille < b.taille){
-        c.taille = b.taille + 1;
-        
-    }
-    
-    else{
-        c.taille = a.taille + 1;
-    }
+    if(a.positif == b.positif){
+        c.positif = a.positif;
+        c.taille = n;
 
-    
-
-    for( int i = 0; i < c.taille; i++){
-        if ((a.digit[i] + b.digit[i] + retenue) >= 10){
-
-            c.digit[i] = (a.digit[i] + b.digit[i] + retenue) % 10;
-            retenue = 1;
-        } 
-
-        else{
-            
-            c.digit[i] = (a.digit[i] + b.digit[i] + retenue);
-            retenue = 0;
-        }
-        
-    }
-
-    afficher_grand_entier(c);
-}
-
-void ordre(GrandEntier a, GrandEntier b, GrandEntier c){
-    if (inferieur_a(a, b) == 1){
-        c.positif = false;
-        soustraire(b, a, c);
-
-    }
-
-    else if (inferieur_a(a, b) == 0){
-        soustraire(a, b, c);
-    }
-}
-
-void soustraire(GrandEntier a, GrandEntier b, GrandEntier c){
-
-    int retenue = 0;
-
-    if (a.taille < b.taille){
-        c.taille = b.taille;
-        
-    }
-    
-
-    else{
-        c.taille = a.taille;
-    }
-
-    for( int i = 0; i < c.taille; i++){
-        if (a.digit[i] < (b.digit[i] + retenue)){
-
-            c.digit[i] = (a.digit[i] + 10 - (b.digit[i] + retenue));
-            retenue = 1;
-
-            if (i == c.taille){
-                c.positif = false;
+        for(int i = 0; i < n; i++){
+            c.digit[i] = a.digit[i] + b.digit[i] + retenue;
+            if (c.digit[i] >= 10) {
+                c.digit[i] -= 10;
+                retenue = 1;
             }
-        } 
-
-        else{
-            
-            c.digit[i] = (a.digit[i] - (b.digit[i] + retenue));
-            retenue = 0;
+            else retenue = 0;
         }
-
+        if(retenue == 1){
+            c.taille += 1;
+            c.digit[c.taille - 1] = 1;
+        }
+    }
+    else{
+        if(a.positif){
+            b.positif = true;
+            return soustraction_sans_signe(a,b);
+        }
+        else{
+            a.positif = true;
+            return soustraction_sans_signe(a,b);
+        }
     }
 
-    afficher_grand_entier(c);
+    return c;
 }
 
-void soustraction(GrandEntier a, GrandEntier b){
+GrandEntier soustraction(GrandEntier a, GrandEntier b){
+    GrandEntier c = negative(b);
+    return addition(a,c);
+}
 
+GrandEntier soustraction_sans_signe(GrandEntier a, GrandEntier b){
     GrandEntier c = {0};
-    c.positif = true;
+    
+    int n = maximum(a.taille, b.taille);
+    int retenue = 0;
+    c.taille = n;
 
-    if(a.positif == 1 && b.positif == 1){
-       ordre(a, b, c);
-    }
 
-    else if (a.positif == 0 && b.positif == 0){
-       ordre(b, a, c);
-    }
+    if(inferieur_a(a, b)){
 
-    else if (a.positif == 1 && b.positif == 0){
-        additionner(a, b, c);
-    }
-
-    else if (a.positif == 0 && b.positif == 1){
         c.positif = false;
-        additionner(a, b, c);
-    }
+        for(int i = 0; i < n; i++){
+            c.digit[i] = b.digit[i] - a.digit[i] - retenue;
 
+            if(c.digit[i] < 0){
+                c.digit[i] += 10;
+                retenue = 1;
+            }
+            else retenue = 0;
+        }
+    }
+    else{
+        c.positif = true;
+        for(int i = 0; i < n; i++){
+            c.digit[i] = a.digit[i] - b.digit[i] - retenue;
+         
+            if(c.digit[i] < 0){
+                c.digit[i] += 10;
+                retenue = 1;
+            }
+            else retenue = 0;
+        }
+    }
+    return c;
 }
 
 int egal_a(GrandEntier a, GrandEntier b){
 
-    int compt = 0;
+    int n = maximum(a.taille, b.taille);
 
-    if (a.positif != b.positif || a.taille != b.taille){
-        return 0;
-    }
-
-    else{
-        for (int i = 0; i < a.taille; i++){
-            if (a.digit[i] != b.digit[i]){
-                compt++;
+    if(a.positif == b.positif){
+        for(int i = 0; i < n; i++){
+            if(a.digit[i] != b.digit[i]){
+                return 0;
             }
         }
-        
-        if (compt == 0){
-            return 1;
-        }
-        else{
-            return 0;
-        }
-    }
-
-}
-
-int different_de(GrandEntier a, GrandEntier b){
-    int k = egal_a(a, b);
-
-    if (k == 0){
         return 1;
     }
     else{
-        return 0;
+        //Signe différent, il faut vérifier que les entiers ne sont pas égals à 0
+        for(int i = 0; i < n; i++){
+            if(a.digit[i] != 0 || b.digit[i] != 0){
+                return 0;
+            }
+        }
+        return 1;
     }
+}
+
+
+int maximum(int a, int b){
+    if(a > b){
+        return a;
+    }
+    else{
+        return b;
+    }
+}
+
+
+int different_de(GrandEntier a, GrandEntier b){
+    return 1 - egal_a(a, b);
 }
 
 int inferieur_a(GrandEntier a, GrandEntier b){
 
-    if (a.positif > b.positif){
-        return 0;
-    }
-    else if(a.positif < b.positif){
-        return 1;
-    }
-    else{
-        if (a.taille < b.taille){
-            return 1;
-        }
-        else if (b.taille < a.taille){
-            return 0;
-        }
-        else{
-            for (int i = a.taille - 1; i >= 0; i--){
-                if (a.digit[i] < b.digit[i]){
-                    return 1;
-                    break;
-                }
-                else if (a.digit[i] > b.digit[i]){
-                    return 0;
-                    break;
-                }
+    int n = maximum(a.taille ,b.taille);
 
-                if (i == 0){
-                    return 0;
-                }
-
+    if(a.positif && b.positif){
+        for(int i = n - 1; i >= 0; i--){
+            if (a.digit[i] < b.digit[i]){
+                return 1;
+            } else if (a.digit[i] > b.digit[i]) {
+                return 0;
             }
         }
+        return 0;
     }
-    return 0;
+    else if(!a.positif && b.positif){
+        for(int i = 0; i < n; i++){
+            if(a.digit[i] != 0 || b.digit[i] != 0){
+                return 1;
+            }
+        }
+        return 0;
+    }
+    else if(a.positif && !b.positif){
+        return 0;
+    }
+    else{
+        for(int i = n - 1; i >= 0; i--){
+            if (a.digit[i] > b.digit[i]){
+                return 1;
+            } else if (a.digit[i] < b.digit[i]) {
+                return 0;
+            }
+        }
+        return 0;
+    }
 }
 
 int inferieur_ou_egal_a(GrandEntier a, GrandEntier b){
-
-    if (a.positif > b.positif){
-        return 0;
-    }
-    else if(a.positif < b.positif){
+    if(inferieur_a(a,b) == 1 || egal_a(a,b) == 1){
         return 1;
     }
-    else{
-        if (a.taille < b.taille){
-            return 1;
-        }
-        else if (b.taille < a.taille){
-            return 0;
-        }
-        else{
-            for (int i = a.taille - 1; i >= 0; i--){
-                if (a.digit[i] < b.digit[i]){
-                    return 1;
-                    break;
-                }
-                else if (a.digit[i] > b.digit[i]){
-                    return 0;
-                    break;
-                }
+    else return 0;
+}
 
-                if (i == 0){
-                    return 1;
-                }
-
-            }
-        }
-    }
-    return 0;
+GrandEntier negative(GrandEntier a){
+    GrandEntier c;
+    memcpy(&c, &a, sizeof(a));
+    c.positif = !a.positif;
+    
+    return c;
 }
 
 
 int main(){
 
-    GrandEntier test = {0};
-
-    test.digit[0] = 7;
-    test.digit[1] = 1;
-    test.digit[2] = 1;
-    test.digit[3] = 1;
-    test.digit[4] = 6;
-    test.digit[5] = 3;
-    test.digit[6] = 8;
-    test.digit[7] = 4;
-    test.digit[8] = 9;
-    test.digit[9] = 2;
-    test.digit[10] = 8;
-    test.digit[11] = 8;
-    test.digit[12] = 9;
-    test.digit[13] = 7;
-    test.digit[14] = 8;
-    test.digit[15] = 1;
-
-    test.taille = 16;
-
-    test.positif = false;
-
-
+    GrandEntier a = lecture_grand_entier("1693476934");
+    GrandEntier b = lecture_grand_entier("-9000");
+        
+    
     printf("Premier entier : \n");
-    afficher_grand_entier(test);
+    afficher_grand_entier(a);
 
 
-    GrandEntier Test2 = {0};        // Initialiser tous les éléments à 0
-                                    // Chaque élément du tableau Test.Digit
-                                    // est "mis à 0", par sécurité
-    Test2.digit[0] = 2;
-    Test2.digit[1] = 1;
-    Test2.digit[2] = 2;
-    Test2.digit[3] = 2;
-    Test2.digit[4] = 3;
-    Test2.digit[5] = 3;
-    Test2.digit[6] = 4;
-    Test2.digit[7] = 4;
-    Test2.digit[8] = 5;
-    Test2.digit[9] = 5;
-    Test2.digit[10] = 6;
-    Test2.digit[11] = 6;
-    Test2.digit[12] = 7;
-    Test2.digit[13] = 7;
-    Test2.digit[14] = 8;
-    Test2.digit[15] = 8;
-    Test2.digit[16] = 9;
-    Test2.digit[17] = 9;
-
-
-    Test2.taille = 18;
-    Test2.positif = true;
-
+    
     printf("Deuxième entier : \n");
-    afficher_grand_entier(Test2);
+    afficher_grand_entier(b);
 
     printf("______________________\n");
     printf("addition : \n");
-    addition(test, Test2);
+    GrandEntier c = addition(a, b);
+    afficher_grand_entier(c);
 
     printf("______________________\n");
     printf("soustraction : \n");
-    soustraction(test, Test2);
+ //   soustraction(a, b);
     
     // printf("a inferieur à b :%d\n", inferieur_a(test, Test2));
 
     // printf("a inferieur ou egal à b :%d\n", inferieur_ou_egal_a(test, Test2));
-
 }
+ 
